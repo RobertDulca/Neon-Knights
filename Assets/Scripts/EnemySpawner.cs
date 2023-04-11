890081c1c3e84b822b9e60afd6acf394a4c98746
johannes.43 <johannes1.seidl@gmx.at>
2023-04-11 15:40:19 +0200

    Anfang von Gegner spawnen
    Sprite für einen Gegner hinzugefügt
    
    EnemySpawn Script erstellt und Code geschrieben der noch nicht funktioniert! Juhu

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public List<GameObject> prefabs;
+
+    public List<Transform> spawnPoints;
+    //Intervall für Gegner spawn (als Variable damit man sie bei Spielfortschritt erhöhen kann)
+    public float spawnInterval = 2f;
+
+
+    public void StartSpawning()
+    {
+        StartCoroutine(SpawnDelay());
+
+        //StopAllCoroutines();
+    }
+
+    IEnumerator SpawnDelay()
+    {
+        //Spawnfunktion Aufrufen
+        SpawnEnemy();
+        //Intervall abwarten
+        yield return new WaitForSeconds(spawnInterval);
+        //Funktion nochmal aufrufen
+        StartCoroutine(SpawnDelay());
+    }
+
+    //Spawnmechanik
+    void SpawnEnemy()
+    {
+        //erstelle zufälligen Gegner -> prefabs.Count zählt Anzahl an Gegner die man einfügt
+        int randomPrefabID = Random.Range(0, prefabs.Count);
+        //entscheide durch Zufall auf welcher Seite der Gegner spawnt
+        int randomSpawnPointID = Random.Range(0, spawnPoints.Count); //0 oder 1
+        //Den Gegner Spawnen
+        GameObject spawnedEnemy = Instantiate(prefabs[randomPrefabID], spawnPoints[randomSpawnPointID]);
+    }
+}
