54956c89bc25e5a3705d3b07326472fa0cf17d3d
johannes.43 <johannes1.seidl@gmx.at>
2023-05-03 04:49:55 +0200

    Gegner laufen auf den Tower zu

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //Leben, Angriffsstärke, Geschwindigkeit
+    public int health, attackPower;
+    public float moveSpeed;
+
+    void Update()
+    {
+        Move();
+    }
+    //Bewegen zum Turm
+    void Move()
+    {
+        transform.Translate(-transform.right * moveSpeed * Time.deltaTime);
+    }
+    //Leben verlieren
+    void LoseHealth()
+    {
+        //Leben abziehen
+        health--;
+        //Hurt animation abspielen/Spritefarbe kurz auf rot setzen
+        StartCoroutine(BlinkRed());
+        //Checken ob Leben = 0, wenn ja Objekt zerstören
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    IEnumerator BlinkRed()
+    {
+        //Die Farbe vom Sprite auf Rot ändern
+        GetComponent<SpriteRenderer>().color = Color.red;
+        //Kurze Zeit warten
+        yield return new WaitForSeconds(0.2f);
+        //Farbe auf Weiß zurück setzen --> Sprite wird normal
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
