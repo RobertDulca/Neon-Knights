9be65278b971982041b8ff358e99b7093d8ddb77
felixgrassl <116748371+felixgrassl@users.noreply.github.com>
2023-05-07 00:07:03 +0200

    Coins added
    Objekte mit dem Tag 'MyCoin' gelten als Coins und erhöhen den Score vom Spieler im playerCoins script, das am Spieler hängt
    +Overlay Text wird upgedated, aber den sieht man irgendwie nicht

---
+++ Assets/Scripts/playerCoins.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public Text MyscoreText;
+    private int ScoreNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreNumber = 0;
+        MyscoreText.text = "Score: " + ScoreNumber;
+    }
+
+    // Update is called once per frame
+   private void OnTriggerEnter2D(Collider2D Coin)
+    {
+        if(Coin.tag == "MyCoin")
+        {
+            ScoreNumber++;
+            Destroy(Coin.gameObject);//destroy when the player touches the coin
+            MyscoreText.text = "Score: " + ScoreNumber;
+        }
+    }
+    
+}
