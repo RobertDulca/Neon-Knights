1a331722bac6f5423076ca231f5ff4d5839ea770
johannes.43 <johannes1.seidl@gmx.at>
2023-05-16 20:01:23 +0200

    Graphics setting button
    Sieht noch nicht gut aus aber funktioniert(glaube ich)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -14,4 +14,9 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
 }
